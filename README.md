1ef074d0817e89415b565d3d2e58b13c28d10f2c
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2025-08-14 15:11:09 -0400

    Webpages are converted to EPUB before being sent. Also cleaned up the code and added a bit of styling.

--- README.md
+++ README.md
@@ -5,18 +5,5 @@ npx wrangler deploy
 
 ---
 
-1. Code (tag) formatting doesn't work on kindle. Fix this
-	* Solution:
-		* search for all `<pre>` tags
-		* split the textcontent by newlines
-		* trim the indent spacing and replace them with `&nbsp;`
-		* insert the new div tag before the existing pre tag
-		* remove the pre tag
-		* the div and paragraph need per-line styling.
-2. Images don't work _at all_. Fix this, please!
-	* The page needs to be compiled into a zip file
-		* each image needs to be within root
-		* each src link should have no dir path
-
 5. Make sure error messages are communicated to the user properly (Especially 'invalid to header' and stuff like that)
 
--- src/index.js
+++ src/index.js
@@ -1,113 +1,106 @@
 import { Readability } from '@mozilla/readability';
 import { parse } from "cookie";
+import getMime from "./mimes"
+import mustache from "mustache";
+import JSZip from "jszip"
 
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
+/*
+ * Sends a mail through the GMail API.
+ * Takes a request and an env object for context.
+ * The request must contain the data for the mail in JSON format.
  */
+async function sendMail(request, env) {
+	const getAsset = (url, request, env) => {
+		return env.assets.fetch(new URL(request.url).origin + '/' + url)
+				.then(response => response.text())
+	}
 
-const content_styles = `
-<style>
-body {
-    margin: 0 25%;
-    padding: 0;
-	color: black;
-	font-family: serif;
-}
+	// Schema:
+	// filename: 	string
+	// content:		string
+	// img_sources:	string[]
+	const data = await request.json()
 
-blockquote {
-	margin: 0;
-	border-left: 4px solid gray;
-	padding-top: 1rem;
-	padding-bottom: 1rem;
-	padding-left: 2rem;
-}
+	// fetching static assets
+	const styling = await getAsset('preview.css', request, env)
+	const mailText = await getAsset('mail.template', request, env);
+	const containerxml = await getAsset('container.xml.template', request, env)
+	const metaopf = await getAsset('meta.opf.template', request, env)
+	const pagexhtml = await getAsset('page.template', request, env)
+	const tocncx = await getAsset('toc.ncx.template', request, env)
 
-code {
-	background: rgba(0,0,0,0.05);
-	width: 100%;
-	padding: 1em;
-	border-radius: 5px;
-	text-wrap: auto;
-}
+	// EPUB file structure:
+	// .
+	// ├── mimetype
+	// ├── toc.ncx
+	// ├── META-INF/
+	// │   └── container.xml
+	// ├── index.html
+	// ├── styles.css
+	// ├── meta.opf
+	// └── images/
+	//		├── *.png
+	//		├── *.jpg
+	//		└── *.webp
 
-h1 {
-    font-weight: 800;
-    font-size: 48px;
-}
+	const uuid = crypto.randomUUID();
+	let zip = new JSZip();
+
+	zip.file("mimetype", "application/epub+zip")
+	zip.file("toc.ncx", mustache.render(tocncx, {
+		uuid: uuid,
+		title: data.filename,
+	}))
+	const meta = zip.folder("META-INF")
+	meta.file("container.xml", mustache.render(containerxml, {}))
+
+	zip.file("index.html", mustache.render(pagexhtml, {
+		title: data.filename,
+		styling: styling,
+		content: data.content,
+	}).replaceAll("&nbsp;", "&#160;"))
+	zip.file("styles.css", styling)
+	zip.file("meta.opf", mustache.render(metaopf, {
+		uuid: uuid,
+		title: data.filename,
+		date: new Date().toISOString(),
+		images: data.img_sources.map((src, idx) => ({
+			src: new URL(src).pathname.split('/').at(-1),
+			id: `id${idx+1}`,
+			mime: getMime(new URL(src).pathname.split('/').at(-1))
+		})),
+	}))
+	const images = zip.folder("images")
+	for (let i = 0; i < data.img_sources.length; i ++) {
+		const src = data.img_sources[i];
+		const image = await fetch(src)
+		const url = new URL(src)
+		const binary = await image.bytes()
+		images.file(url.pathname.split('/').at(-1), binary)
+	}
+
+	// generate the zip file
+	const zipContent = await zip.generateAsync({type: "base64", mimeType: "application/epub+zip"})
+	data.zipContent = zipContent
+
+	// generate email body
+	const body = mustache.render(mailText, data)
 
 
-p {
-    font-size: 16px;
-}
-
-hr {
-    border-top: solid 2px;
-	margin: 2rem 0;
-	width: 100%;
-}
-
-a {
-	color: black;
-    font-weight: 700;
-    text-decoration: underline;
-}
-
-code {
-	display: flex;
-	flex-direction: column;
-}
-</style>
-
-`;
-
-async function getPage(url) {
-	const response = await fetch(url)
-	const body = await response.text()
-	return body
-}
-
-async function sendMail(request) {
+	// get oauth
 	const cookies = parse(request.headers.get("Cookie") || "");
 	if (!cookies['oauth-params']) {
 		return new Error('Not logged in.');
 	}
 	const token = JSON.parse(cookies['oauth-params']).access_token
+	const gapiMailEndpoint = `https://gmail.googleapis.com/upload/gmail/v1/users/me/messages/send?uploadType=multipart`
 
-	const data = await request.json()
-	const url = `https://gmail.googleapis.com/upload/gmail/v1/users/me/messages/send?uploadType=multipart`
-	const body = `Content-Type: multipart/mixed; boundary=foo_bar_baz
-MIME-Version: 1.0
-To: ${data.kindle_email}
-subject: Kindle Mail
-
---foo_bar_baz
-Content-Type: text/html; charset="UTF-8"
-MIME-Version: 1.0
-
-<p>Document Attached Below.</p>
-
---foo_bar_baz
-Content-Type: text/html
-MIME-Version: 1.0
-Content-Transfer-Encoding: base64
-Content-Disposition: attachment; filename="${data.filename}.html"
-
-${btoa((content_styles + data.content).replace(/[^\x20-\x7E]/g, ""))}
-
---foo_bar_baz--
-`
-
+	// invoke GMail API to send the generated email
 	// required data keys: content, kindle_email & filename
 	try {
 		if (cookies['oauth-params']) {
 			console.log("sending mail... length", body.length)
-			const response = await fetch(url, {
+			const response = await fetch(gapiMailEndpoint, {
 				method: "POST",
 				headers: {
 					'Content-Type': 'message/rfc822',
@@ -127,12 +120,14 @@ ${btoa((content_styles + data.content).replace(/[^\x20-\x7E]/g, ""))}
 }
 
 export default {
-	async fetch(request) {
+	async fetch(request, env) {
 		const url = new URL(request.url);
 		const cookies = parse(request.headers.get("Cookie") || "");
+
 		switch (url.pathname) {
 			case '/loginstatus':
 				if (cookies['oauth-params']) {
+					// check if oauth token is valid, if it is present
 					const oauth = JSON.parse(cookies['oauth-params'])
 					const currentSeconds = Math.floor(Date.now() / 1000);
 					if (oauth.iat + oauth.expires_in > currentSeconds) {
@@ -140,20 +135,24 @@ export default {
 					}
 				}
 				return new Response('nok')
+
 			case '/setcookie':
 				const body = await request.text()
 				let headers = new Headers()
 				headers.append("Set-Cookie", `oauth-params=${body}; Path=/; HttpOnly; SameSite=Strict; Secure;`)
 				return new Response('ok', { headers: headers} );
+
 			case '/upload':
-				const err = await sendMail(request);
+				const err = await sendMail(request, env);
 				if (err != null) {
 					return new Response(err.message)
 				}
 				return new Response('ok')
+
 			case '/url':
 				const usearch = (new URLSearchParams((new URL(request.url)).search)).get('url')
-				return new Response(await getPage(usearch))
+				return new Response(await fetch(usearch).then(response => response.text()))
+
 			default:
 				return new Response('Not Found', { status: 404 });
 		}
